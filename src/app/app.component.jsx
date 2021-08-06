596639406b382d6cbabe140478ce1bd25b29a1ba
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-08-06 17:54:36 +0200

    chore: initialize project

---
+++ src/app/app.component.jsx
@@ -0,0 +1,3 @@
+import './app.styles.css';
+
+export const App = () => <h1>react-javascript-boilerplate</h1>;
---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import { render } from 'react-dom';
+
+import { App } from './app/app.component';
+
+render(<App />, document.getElementById('root'));
