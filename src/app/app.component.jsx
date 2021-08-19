a445b2694d47924549e3468e55020767d4e9e65d
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-08-19 22:28:42 +0200

    chore: add setup-project script

--- src/app/app.component.jsx
+++ src/app/app.component.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 
 import cls from './app.styles.css';
 
-export const App = () => <h1 className={cls.title}>{'react-javascript-boilerplate'}</h1>;
+export const App = () => <h1 className={cls.title}>{'<app-shortcut>'}</h1>;
