5a5dc481555a2761e1e8d1b75831c72635625ed7
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2021-08-31 20:34:31 +0200

    chore: update shell scripts (#12)

--- scripts/git-checkout-main-with-pull.sh
+++ scripts/git-checkout-main-with-pull.sh
@@ -1,2 +1,8 @@
-git checkout main
-git pull origin main
+mainBranchName=$(git remote show origin | sed -n '/HEAD branch/s/.*: //p')
+echo "Info: Detach main branch name as: '$mainBranchName'."
+
+git checkout $mainBranchName
+echo "Info: Checkout to '$mainBranchName' branch."
+
+git pull origin $mainBranchName
+echo "Info: Pull changes from '$mainBranchName' branch."
